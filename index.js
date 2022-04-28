ef0a9e5f00444c8b250c952804afc66c71decf00
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-28 11:43:07 +0600

    GET API Done

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 4000;
 
-
 // Middlewares
-app.use(cors()) 
-app.use(express.json()) //--- to parse the data received from client side
+app.use(cors());
+app.use(express.json()); //--- to parse the data received from client side
 
-// 1. GET
-// 2. POST
-// 3. DELETE
-// 4. PUT
+const uri =
+  `mongodb+srv://riyad:${process.env.PASS}@curdoperation.qbqwp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("myDB").collection("user");
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    
+    // 1. GET
+    app.get('/user', async (request, response) => {
+        const cursor = userCollection.find({})
+        const result = await cursor.toArray()
+        response.send(result)
+    })
+      
+      
+    // 2. POST
+    // 3. DELETE
+    // 4. PUT
+      
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
   console.log(`Our Server is running`);
